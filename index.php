f787eb988fcda9d87ed54555619d2ac87b92170a
Szymon100 <szymon.bartus04@gmail.com>
2024-02-27 15:48:37 +0100

    Front-end toegevoegd

--- index.php
+++ index.php
@@ -4,13 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="Front-end.css" rel="stylesheet"/>
 </head>
 <body>
-    <p>text<p>
-    <?php
-        if (false){
-            echo "wow";
-        }
-    ?>
-</body>
+        <h1>Titel</h1>
+        <nav>
+        <ul>
+        <li><a href="home.html">home</a></li>
+        <li><a href="populair.html">Populaire</a></li>
+        <li><a href="tags.html">Tags</a></li>
+        <li><a href="overons.html">Over Ons</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="inloggen.html">Inloggen</a></li>
+        </ul>
+        </nav>
+        <br>
+        <p class="Z1">
+            <label for="zoek">Wat voor vandaag?</label>
+            <input type="text" name="tekstveld1" id="zoek" placeholder="Tag">
+        </p>
+        <br>
+        <h3 class="P1">De populairste recept:</h3>
+        #float
+        <img class="F1" src="C:\Users\r1000209\Documents\Practise Enterprice\Best-chocolate-cake-recipe-1-of-1-2.jpg" alt="een foto">
+        <span>Kleine synopsis van recept of tags van de recept</span>
+        
+        <hr>
+        <h3>Kijk ook</h3>
+        <hr>
+        <h3>Makkelijke recepten</h3>
+        <footer>
+        <hr>
+        <ul>
+            <li>Home</li>
+            <li>Populaire</li>
+            <li>Tags</li>
+            <li>Over Ons</li>
+            <li>Contact</li>
+            <li>Inloggen</li>
+        </ul>
+        </footer>
+    </body>
 </html>
