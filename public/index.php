4b7945f8cfe23671291ef071f0e8897dbfbcbf5e
Szymon100 <szymon.bartus04@gmail.com>
2024-03-01 15:29:31 +0100

    Index benoeming

--- public/index.php
+++ public/index.php
@@ -8,8 +8,8 @@
 </head>
     <body>
         <div class="wrapper">
-            <div class="D1">      
-                <nav class="N1">
+            <div class="div1">      
+                <nav class="nav1">
                     <ul>
                         <li><h1><a href="index.php">Chefly</a></h1></li>
                         <li><a href="home.html">Home</a></li>
@@ -19,7 +19,7 @@
                         <li><a href="contact.html">Contact</a></li>
                         <li><a href="inloggen.html">Inloggen</a></li>
                         <li>                
-                            <p class="Z1">
+                            <p class="zoek1">
                             <label for="zoek"></label>
                             <input type="text" name="tekstveld1" id="zoek" placeholder="zoeken">
                             </p>
@@ -29,27 +29,27 @@
             </div>
                 <div class="content">
 
-                <h3 class="P1">Het populairste recept:</h3>
-                #float
-                <img class="F1" src="C:\Users\r1000209\Documents\Practise Enterprice\Best-chocolate-cake-recipe-1-of-1-2.jpg" alt="een foto">
-                <span>Kleine synopsis van recept of tags van de recept</span>
-                
-                
-                <h3>Kijk ook</h3>
-                
-                <h3>Snelle recepten</h3>
+                    <h3 class="par1">Het populairste recept:</h3>
+                    #float
+                    <img class="foto1" src="C:\Users\r1000209\Documents\Practise Enterprice\Best-chocolate-cake-recipe-1-of-1-2.jpg" alt="een foto">
+                    <span>Kleine synopsis van recept of tags van de recept</span>
+                    
+                    
+                    <h3>Kijk ook</h3>
+                    
+                    <h3>Snelle recepten</h3>
 
-            </div>
+                </div>
 
             <footer>
-            <ul>
-                <li>Home</li>
-                <li>Populaire</li>
-                <li>Tags</li>
-                <li>Over Ons</li>
-                <li>Contact</li>
-                <li>Inloggen</li>
-            </ul>
+                <ul>
+                    <li>Home</li>
+                    <li>Populaire</li>
+                    <li>Tags</li>
+                    <li>Over Ons</li>
+                    <li>Contact</li>
+                    <li>Inloggen</li>
+                </ul>
             </footer>
 
         </div>
