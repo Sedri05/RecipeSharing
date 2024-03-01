007a34ad920fb004f1c2311a360f33c084336ba9
Senne <sedri051@gmail.com>
2024-03-01 15:44:20 +0100

    Centered top bar

--- public/index.php
+++ public/index.php
@@ -1,57 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="Front-end.css" rel="stylesheet"/>
+    <link href="Front-end.css" rel="stylesheet" />
 </head>
-    <body>
-        <div class="wrapper">
-            <div class="div1">      
-                <nav class="nav1">
-                    <ul>
-                        <li><h1><a href="index.php">Chefly</a></h1></li>
-                        <li><a href="home.html">Home</a></li>
-                        <li><a href="populair.html">Populaire</a></li>
-                        <li><a href="tags.html">Tags</a></li>
-                        <li><a href="overons.html">Over Ons</a></li>
-                        <li><a href="contact.html">Contact</a></li>
-                        <li><a href="inloggen.html">Inloggen</a></li>
-                        <li>                
-                            <p class="zoek1">
+
+<body>
+    <div class="wrapper">
+        <div class="div1">
+            <nav class="nav1">
+                <div class="flex">
+                    <div class="flex-item">
+                        <h1><a href="index.php">Chefly</a></h1>
+                    </div>
+                    <div class="flex-item"><a href="home.html">Home</a></div>
+                    <div class="flex-item"><a href="populair.html">Populaire</a></div>
+                    <div class="flex-item"><a href="tags.html">Tags</a></div>
+                    <div class="flex-item"><a href="overons.html">Over Ons</a></div>
+                    <div class="flex-item"><a href="contact.html">Contact</a></div>
+                    <div class="flex-item"><a href="inloggen.html">Inloggen</a></div>
+                    <div class="flex-item">
+                        <p class="zoek1">
                             <label for="zoek"></label>
                             <input type="text" name="tekstveld1" id="zoek" placeholder="zoeken">
-                            </p>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-                <div class="content">
-
-                    <h3 class="par1">Het populairste recept:</h3>
-                    #float
-                    <img class="foto1" src="C:\Users\r1000209\Documents\Practise Enterprice\Best-chocolate-cake-recipe-1-of-1-2.jpg" alt="een foto">
-                    <span>Kleine synopsis van recept of tags van de recept</span>
-                    
-                    
-                    <h3>Kijk ook</h3>
-                    
-                    <h3>Snelle recepten</h3>
-
+                        </p>
+                    </div>
                 </div>
+            </nav>
+        </div>
+        
+        <div class="content">
 
-            <footer>
-                <ul>
-                    <li>Home</li>
-                    <li>Populaire</li>
-                    <li>Tags</li>
-                    <li>Over Ons</li>
-                    <li>Contact</li>
-                    <li>Inloggen</li>
-                </ul>
-            </footer>
+            <h3 class="par1">Het populairste recept:</h3>
+            #float
+            <img class="foto1" src="C:\Users\r1000209\Documents\Practise Enterprice\Best-chocolate-cake-recipe-1-of-1-2.jpg" alt="een foto">
+            <span>Kleine synopsis van recept of tags van de recept</span>
+
+
+            <h3>Kijk ook</h3>
+
+            <h3>Snelle recepten</h3>
 
         </div>
-    </body>
+
+        <footer>
+            <ul>
+                <li>Home</li>
+                <li>Populaire</li>
+                <li>Tags</li>
+                <li>Over Ons</li>
+                <li>Contact</li>
+                <li>Inloggen</li>
+            </ul>
+        </footer>
+
+    </div>
+</body>
+
 </html>
