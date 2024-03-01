58e6c10f6c4690ddb3c074f4a4e9eb9be33a341c
Szymon100 <szymon.bartus04@gmail.com>
2024-03-01 15:23:22 +0100

    Index toplayout

--- public/index.php
+++ public/index.php
@@ -10,30 +10,24 @@
         <div class="wrapper">
             <div class="D1">      
                 <nav class="N1">
-                <ul>
-                <li><h1><a href="index.php">Chefly</a></h1></li>
-                <li><a href="home.html">Home</a></li>
-                <li><a href="populair.html">Populaire</a></li>
-                <li><a href="tags.html">Tags</a></li>
-                <li><a href="overons.html">Over Ons</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <li><a href="inloggen.html">Inloggen</a></li>
-                <li>                
-                    <p class="Z1">
-                    <label for="zoek"></label>
-                    <input type="text" name="tekstveld1" id="zoek" placeholder="zoeken">
-                    </p>
-                </li>
-                </ul>
+                    <ul>
+                        <li><h1><a href="index.php">Chefly</a></h1></li>
+                        <li><a href="home.html">Home</a></li>
+                        <li><a href="populair.html">Populaire</a></li>
+                        <li><a href="tags.html">Tags</a></li>
+                        <li><a href="overons.html">Over Ons</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="inloggen.html">Inloggen</a></li>
+                        <li>                
+                            <p class="Z1">
+                            <label for="zoek"></label>
+                            <input type="text" name="tekstveld1" id="zoek" placeholder="zoeken">
+                            </p>
+                        </li>
+                    </ul>
                 </nav>
             </div>
                 <div class="content">
-            
-                <nav>
-                <br>
-                <br>
-                </nav>
-                <br>
 
                 <h3 class="P1">Het populairste recept:</h3>
                 #float
