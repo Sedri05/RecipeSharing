ee48b15a13ac2d632fd05a0b801df77f2c2bb7a4
Senne <sedri051@gmail.com>
2024-03-01 14:19:06 +0100

    Footer file

---
+++ public/footer.php
@@ -0,0 +1,12 @@
+<?php echo "<footer>
+        
+        <ul>
+            <li>Home</li>
+            <li>Populaire</li>
+            <li>Tags</li>
+            <li>Over Ons</li>
+            <li>Contact</li>
+            <li>Inloggen</li>
+        </ul>
+        </footer>"
+?>
