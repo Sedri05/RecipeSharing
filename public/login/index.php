ed81a54200f21d752d2bbc3945e98bd2f485414b
Senne <sedri051@gmail.com>
2024-03-05 19:40:47 +0100

    Comments voor front end

--- public/login/index.php
+++ public/login/index.php
@@ -14,12 +14,12 @@
     <div class="container">
         <label for="email">E-mail</label>
         <input type="text" id="email"> </input>
-        <p id="email_error"></p>
+        <p id="email_error"></p> <!--Hier komen email errors zoals, geen of incorrecte email. Maak dit rood-->
         <label for="password">Password</label>
         <input class="" type="password" id="password"> </input>
-        <p id="password_error"></p>
+        <p id="password_error"></p> <!--Hier komen wachtwoord errors zoals, geen of incorrect wachtwoord. Maak dit rood-->
         <input type="button" onclick="login()" value="Log in"></input>
-        <p id="general_error"></p>
+        <p id="general_error"></p> <!--Hier komen general errors. Maak dit rood-->
     </div>
 </body>
 
