7a1ca71c88ccb33c973fa0211b0dbf345efea568
Senne <sedri051@gmail.com>
2024-03-06 17:15:35 +0100

    Header update

--- public/header.php
+++ public/header.php
@@ -1,24 +1,22 @@
 <link href="/header-style.css" rel="stylesheet" />
-<div class="wrapper">
-    <div class="div1">
-        <nav class="nav1">
-            <div class="flex">
-                <div class="flex-item">
-                    <h1><a href="index.php">Chefly</a></h1>
-                </div>
-                <div class="flex-item"><a href="home.html">Home</a></div>
-                <div class="flex-item"><a href="populair.html">Populaire</a></div>
-                <div class="flex-item"><a href="tags.html">Tags</a></div>
-                <div class="flex-item"><a href="overons.html">Over Ons</a></div>
-                <div class="flex-item"><a href="contact.html">Contact</a></div>
-                <div class="flex-item"><a href="inloggen.html">Inloggen</a></div>
-                <div class="flex-item">
-                    <p class="zoek1">
-                        <label for="zoek"></label>
-                        <input type="text" name="tekstveld1" id="zoek" placeholder="zoeken">
-                    </p>
-                </div>
+
+<div class="header-container">
+    <nav class="header-nav">
+        <div class="header-flex">
+            <div class="header-flex-item">
+                <h1><a href="index.php">Chefly</a></h1>
             </div>
-        </nav>
-    </div>
+            <div class="header-flex-item"><a href="/">Home</a></div>
+            <div class="header-flex-item"><a href="/">Populaire</a></div>
+            <div class="header-flex-item"><a href="/search">Tags</a></div>
+            <div class="header-flex-item"><a href="/about">Over Ons</a></div>
+            <div class="header-flex-item"><a href="/login">Inloggen</a></div>
+            <div class="header-flex-item">
+                <p class="header-zoek">
+                    <label for="zoek"></label>
+                    <input type="text" name="tekstveld1" id="zoek" placeholder="zoeken">
+                </p>
+            </div>
+        </div>
+    </nav>
 </div>
