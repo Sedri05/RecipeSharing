4aaf6cd9c05f7ef0b567e753a1df2e64ead6f0c3
Senne <sedri051@gmail.com>
2024-03-06 17:18:02 +0100

    Index fixup

--- public/index.php
+++ public/index.php
@@ -9,36 +9,32 @@
 </head>
 
 <body>
-    <?php 
-        require("header.php")
-    ?>
-        
-        <div class="content">
+    <?php require("header.php") ?>
 
-            <h3 class="par1">Het populairste recept:</h3>
-            #float
-            <img class="foto1" src="C:\Users\r1000209\Documents\Practise Enterprice\Best-chocolate-cake-recipe-1-of-1-2.jpg" alt="een foto">
-            <span>Kleine synopsis van recept of tags van de recept</span>
+    <div class="content">
+
+        <h3 class="par1">Het populairste recept:</h3>
+        #float
+        <img class="foto1" src="C:\Users\r1000209\Documents\Practise Enterprice\Best-chocolate-cake-recipe-1-of-1-2.jpg" alt="een foto">
+        <span>Kleine synopsis van recept of tags van de recept</span>
 
 
-            <h3>Kijk ook</h3>
+        <h3>Kijk ook</h3>
 
-            <h3>Snelle recepten</h3>
-
-        </div>
-
-        <footer>
-            <ul>
-                <li>Home</li>
-                <li>Populaire</li>
-                <li>Tags</li>
-                <li>Over Ons</li>
-                <li>Contact</li>
-                <li>Inloggen</li>
-            </ul>
-        </footer>
+        <h3>Snelle recepten</h3>
 
     </div>
+
+    <footer>
+        <ul>
+            <li>Home</li>
+            <li>Populaire</li>
+            <li>Tags</li>
+            <li>Over Ons</li>
+            <li>Contact</li>
+            <li>Inloggen</li>
+        </ul>
+    </footer>
 </body>
 
 </html>
