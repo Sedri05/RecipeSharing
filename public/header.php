aad73e4267e39a83e1efb38edbe555fb252c65a2
Szymon100 <szymon.bartus04@gmail.com>
2024-03-08 14:03:56 +0100

    Search balk aangepast

--- public/header.php
+++ public/header.php
@@ -1,5 +1,5 @@
 <link href="/header-style.css" rel="stylesheet" />
-
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 <div class="header-container">
     <nav class="header-nav">
         <div class="header-flex">
@@ -11,8 +11,9 @@
             <div class="header-flex-item"><a href="/login">Inloggen</a></div>
             <div class="header-flex-item-search">
                 <p class="header-zoek">
+                    <span class="search-icon material-symbols-outlined">search</span>
                     <label for="zoek"></label>
-                    <input type="text" name="tekstveld1" id="zoek" placeholder="zoeken">
+                    <input class="search-input" type="text" placeholder="zoeken">
                 </p>
             </div>
         </div>
