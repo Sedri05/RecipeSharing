25a8daa93a355a00ce4c89f5130da0b913306b91
Senne <sedri051@gmail.com>
2024-03-22 14:43:13 +0100

    Footer recept pagina

--- public/account/recepten.php
+++ public/account/recepten.php
@@ -4,14 +4,23 @@ $user = $_SESSION["user"];
 require_once("../../private/database.php");
 $database = new Database();
 
-$recepten = $database->select("SELECT `ReceptID`, `Title`, `Personen`, `Moeilijkheid`, `Berijdingstijd`, `Foto`, `MaaltijdtypeID`, `Date` FROM `recept` WHERE `GebruikerID` = ?", ["i", [$user]], false);
+$recepten = $database->select(
+    "SELECT `ReceptID`, `Title`, `Personen`, `Moeilijkheid`, `Berijdingstijd`, `Foto`, m.`Maaltijdtype`, `Date` FROM `recept` r LEFT JOIN `maaltijdtype` m ON r.`MaaltijdtypeID` = m.`MaaltijdtypeID` WHERE `GebruikerID` = ?",
+    ["i", [$user]]
+);
 
-foreach ($recepten as $recept){
-    ?>
+foreach ($recepten as $recept) {
+?>
     <div class="info-text">
-            <h1> <?php echo $recept["Title"]?>
+        <h2 class="recept-title"> <?php echo $recept["Title"] ?> </h2>
+        <div class="recept-info">
+            <div class="info-button"> Personen: <?php echo $recept["Personen"] ?> </div>
+            <div class="info-button"> Moeilijkheid: <?php echo $recept["Moeilijkheid"] ?>/5 </div>
+            <div class="info-button"> Bereidingstijd: <?php echo $recept["Berijdingstijd"] ?>mins </div>
+            <div class="info-button"> Maaltijdtype: <?php echo $recept["Maaltijdtype"] ?> </div>
+        </div>
     </div>
-    <?php
+<?php
 }
 
 ?>
--- public/recept/index.php
+++ public/recept/index.php
@@ -11,89 +11,89 @@
 </head>
 
 <body>
-    <?php require("../header.php");
-    if (!isset($_GET["recept"])) { ?>
-        <p> 404 Site not found </p>
-    <?php die();
-    }
-    require_once "../../private/database.php";
-    $recept_id = $_GET["recept"];
-    $database = new Database();
-    $recept_info = $database->select_one("SELECT * FROM `recept` WHERE `ReceptID` = ?", ["i", [$recept_id]], false);
-    $recept_tags = $database->select("SELECT `Tagname` FROM `tag` t LEFT JOIN `tagsrecept` r ON t.TagID = r.TagID WHERE r.ReceptID = ?", ["i", [$recept_id]], false);
-    $ingredients = $database->select("SELECT `Ingredient` FROM `ingredient` i LEFT JOIN `ingredientrecept` r ON i.IngredientID = r.IngredientID WHERE r.ReceptID = ?", ["i", [$recept_id]], false);
-    $creator = $database->select_one("SELECT `Naam`, `Achternaam` FROM `gebruiker` WHERE `GebruikerID` = ?", ["i", [$recept_info["GebruikerID"]]], false);
-    $review = $database->select("SELECT * FROM `review` WHERE `ReceptID` = ?", ["i", [$recept_id]], false);
-    $type = $database->select_one("SELECT `Maaltijdtype` FROM `maaltijdtype` WHERE `MaaltijdtypeID` = ?;", ["i", [$recept_info["MaaltijdtypeID"]]]);
-    ?>
-    <div class="centered">
-        <div class="container">
-            <div class="title-div">
-                <div class="title">
-                    <h1> <?php echo $recept_info["Title"]; ?> </h1>
-                </div>
-                <div class="creator">
-                    <p> <?php echo $creator["Naam"] . " " . $creator["Achternaam"] ?> </p>
-                </div>
-                <div class="maaltijdtype">
-                    <p> <?php echo $type["Maaltijdtype"]; ?> </p>
-                </div>
-                <div class="Bereidingstijd">
-                    <p> <?php echo $recept_info["Berijdingstijd"]; ?>min. </p>
-                </div>
-                <div class="Moeilijkheid">
-                    <p> Moeilijkheid: <?php echo $recept_info["Moeilijkheid"]; ?>/5 </p>
-                </div>
-            </div>
-            <div class="ingr-tags-image">
-                <div class="recept-image">
-                    <img class="image" src="/black-cake-2.jpg" alt="dit is een test foto"/>
-                </div>
-                <div class="ingr-tags">
-                    <div class="scrollmenu-div">
-                        <p class="scrollmenu-title">Tags</p>
-                        <div class="scrollmenu">
-                            <?php
-                            foreach ($recept_tags as $tag) {
-                                echo "<p> " . $tag["Tagname"] . "</p>";
-                            }
-                            ?>
-                        </div>
+    <div class="wrapper">
+        <?php require("../header.php");
+        if (!isset($_GET["recept"])) { ?>
+            <p> 404 Site not found </p>
+        <?php die();
+        }
+        require_once "../../private/database.php";
+        $recept_id = $_GET["recept"];
+        $database = new Database();
+        $recept_info = $database->select_one("SELECT * FROM `recept` WHERE `ReceptID` = ?", ["i", [$recept_id]], false);
+        $recept_tags = $database->select("SELECT `Tagname` FROM `tag` t LEFT JOIN `tagsrecept` r ON t.TagID = r.TagID WHERE r.ReceptID = ?", ["i", [$recept_id]], false);
+        $ingredients = $database->select("SELECT `Ingredient` FROM `ingredient` i LEFT JOIN `ingredientrecept` r ON i.IngredientID = r.IngredientID WHERE r.ReceptID = ?", ["i", [$recept_id]], false);
+        $creator = $database->select_one("SELECT `Naam`, `Achternaam` FROM `gebruiker` WHERE `GebruikerID` = ?", ["i", [$recept_info["GebruikerID"]]], false);
+        $review = $database->select("SELECT * FROM `review` WHERE `ReceptID` = ?", ["i", [$recept_id]], false);
+        $type = $database->select_one("SELECT `Maaltijdtype` FROM `maaltijdtype` WHERE `MaaltijdtypeID` = ?;", ["i", [$recept_info["MaaltijdtypeID"]]]);
+        ?>
+        <div class="centered">
+            <div class="container">
+                <div class="title-div">
+                    <div class="title">
+                        <h1> <?php echo $recept_info["Title"]; ?> </h1>
                     </div>
-                    <div class="scrollmenu-div">
-                        <p class="scrollmenu-title">Ingredienten</p>
-                        <div class="scrollmenu">
-                            <?php
-                            foreach ($ingredients as $ingredient) {
-                                echo "<p> " . $ingredient["Ingredient"] . "</p>";
-                            }
-                            ?>
-                        </div>
+                    <div class="creator">
+                        <p> <?php echo $creator["Naam"] . " " . $creator["Achternaam"] ?> </p>
+                    </div>
+                    <div class="maaltijdtype">
+                        <p> <?php echo $type["Maaltijdtype"]; ?> </p>
+                    </div>
+                    <div class="Bereidingstijd">
+                        <p> <?php echo $recept_info["Berijdingstijd"]; ?>min. </p>
+                    </div>
+                    <div class="Moeilijkheid">
+                        <p> Moeilijkheid: <?php echo $recept_info["Moeilijkheid"]; ?>/5 </p>
                     </div>
                 </div>
-            </div>
-            
-            <div class="bereiding-div">
-                <p class="bereiding-title">Bereiding</p>
-                <div class="bereiding">
-                    <p>
-                        <?php echo $recept_info["Bereiding"]; ?>
-                    </p>
+                <div class="ingr-tags-image">
+                    <div class="recept-image">
+                        <img class="image" src="/black-cake-2.jpg" alt="dit is een test foto" />
+                    </div>
+                    <div class="ingr-tags">
+                        <div class="scrollmenu-div">
+                            <p class="scrollmenu-title">Tags</p>
+                            <div class="scrollmenu">
+                                <?php
+                                foreach ($recept_tags as $tag) {
+                                    echo "<p> " . $tag["Tagname"] . "</p>";
+                                }
+                                ?>
+                            </div>
+                        </div>
+                        <div class="scrollmenu-div">
+                            <p class="scrollmenu-title">Ingredienten</p>
+                            <div class="scrollmenu">
+                                <?php
+                                foreach ($ingredients as $ingredient) {
+                                    echo "<p> " . $ingredient["Ingredient"] . "</p>";
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="bereiding-div">
+                    <p class="bereiding-title">Bereiding</p>
+                    <div class="bereiding">
+                        <p>
+                            <?php echo $recept_info["Bereiding"]; ?>
+                        </p>
+                    </div>
+                </div>
+                <div class="review-div">
+                    <p class="review-title">Reviews</p>
+                    <div class="review">
+                        <p>
+
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div class="review-div">
-                <p class="review-title">Reviews</p>
-                <div class="review">
-                    <p>
-                        
-                    </p>
-                </div>
-            </div>
-            <?php require("footer.php") ?>
         </div>
     </div>
-         
-    <?php require("../footer.php") ?>
+    <?php require("../footer.php"); ?>
 </body>
 
 </html>
