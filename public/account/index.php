d1863823b7b46b8813ce550f79441611fd35f44b
Senne <sedri051@gmail.com>
2024-03-22 13:47:36 +0100

    Account page footer changes

--- public/account/index.php
+++ public/account/index.php
@@ -12,7 +12,6 @@
 </head>
 
 <body>
-    <?php require("../header.php"); ?>
     <?php if (!isset($_SESSION["logged_in"])) { ?>
         <p> You are not logged in. Click <a href="../login/">Here</a> to log in.</p>
     <?php
@@ -29,32 +28,34 @@
     $masked_local_part = strlen($local_part) > 2 ? substr($local_part, 0, 2) . str_repeat("*", strlen($local_part) - 2) : "**";
     $email = $masked_local_part . "@" . $email_parts[1];
     ?>
-
-    <div class="container">
-        <div class="action-select">
-            <button selected id="informatie" onclick="getAction('informatie', this)">Mijn informatie</button>
-            <button id="verander" onclick="getAction('veranderInformatie', this)">Verander informatie</button>
-            <button id="recepten" onclick="getAction('recepten', this)">Mijn recepten</button>
-            <button id="favorieten" onclick="getAction('veranderInformatie', this)">Favorieten</button>
-            <button id="delete" onclick="getAction('veranderInformatie', this)">Verwijder Account</button>
-            <button id="logout" onclick="logout()">Log Out</button>
-        </div>
-        <div class="info" id="info">
-            <div class="info-text">
-                <h2> Naam </h2>
-                <p> <?php echo  $user["Naam"] . " " . $user["Achternaam"] ?> </p>
+    <div class="wrapper">
+        <?php require("../header.php"); ?>
+        <div class="container">
+            <div class="action-select">
+                <button selected id="informatie" onclick="getAction('informatie', this)">Mijn informatie</button>
+                <button id="verander" onclick="getAction('veranderInformatie', this)">Verander informatie</button>
+                <button id="recepten" onclick="getAction('recepten', this)">Mijn recepten</button>
+                <button id="favorieten" onclick="getAction('veranderInformatie', this)">Favorieten</button>
+                <button id="delete" onclick="getAction('veranderInformatie', this)">Verwijder Account</button>
+                <button id="logout" onclick="logout()">Log Out</button>
             </div>
-            <div class="info-text">
-                <h2> Email </h2>
-                <p> <?php echo $email ?> </p>
-            </div>
-            <div class="info-text">
-                <h2> Join Date </h2>
-                <p> <?php echo date_format(date_create($user["Joindate"]), "F jS Y H:i:s") ?> </p>
+            <div class="info" id="info">
+                <div class="info-text">
+                    <h2> Naam </h2>
+                    <p> <?php echo  $user["Naam"] . " " . $user["Achternaam"] ?> </p>
+                </div>
+                <div class="info-text">
+                    <h2> Email </h2>
+                    <p> <?php echo $email ?> </p>
+                </div>
+                <div class="info-text">
+                    <h2> Join Date </h2>
+                    <p> <?php echo date_format(date_create($user["Joindate"]), "F jS Y H:i:s") ?> </p>
+                </div>
             </div>
         </div>
     </div>
-
+    <?php require("../footer.php"); ?>
 </body>
 
 </html>
