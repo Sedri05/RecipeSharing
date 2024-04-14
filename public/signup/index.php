a0102c25256e16ead5d76a4d8e9764340ff7acf7
Szymon100 <szymon.bartus04@gmail.com>
2024-04-14 17:24:55 +0200

    Sign up style.css afgemaakt
    Senne kijk is na de code of script van "ik heb al een account want ik weet niet of is dat een bedoeling maar om naar login pagina te gaan moet je twee keer klikken voordat je gaat naar de pagina

--- public/signup/index.php
+++ public/signup/index.php
@@ -9,28 +9,34 @@
     <link href="style.css" rel="stylesheet" />
 </head>
 <body>
-    <?php require("../header.php")?>
-    <div class="container">
-        <label for="firstname">First Name</label>
-        <input type="text" id="firstname"> </input>
-        <p id="firstname_error"></p> <!--Errors hier-->
-        <label for="lastname">Last Name</label>
-        <input type="text" id="lastname"> </input>
-        <p id="lastname_error"></p> <!--Errors hier-->
-        <label for="email">E-mail</label>
-        <input type="text" id="email"> </input>
-        <p id="email_error"></p> <!--Hier komen email errors zoals, geen of incorrecte email. Maak dit rood-->
-        <label for="password">Password</label>
-        <input class="" type="password" id="password" oninput="checkPassword()"> </input>
-        <p id="password_error"></p> <!--Hier komen wachtwoord errors zoals, geen of incorrect wachtwoord. Maak dit rood-->
-        <label for="confirm_password">Confirm Password</label>
-        <input type="password" id="confirm_password" oninput="checkPassword()"> </input>
-        <p id="confirm_password_error"></p>  <!--Errors hier-->
-        <p>Tos hier?</p>
-        <input type="button" onclick="signup()" value="Sign Up"></input>
-        <p id="general_error"></p> <!--Hier komen general errors. Maak dit rood-->
-        <a href="../login">Ik heb al een account.</a>
-        <?php require("footer.php") ?>
+    <div class="wrapper">
+        <?php require("../header.php")?>
+        <div class="container">
+            <div class="navigation">
+                <label class="FirstName" for="firstname">First Name</label>
+                <input class="text" type="text" id="firstname"> </input>
+                <p id="firstname_error"></p> <!--Errors hier-->
+                <label class="LastName" for="lastname">Last Name</label>
+                <input class="text" type="text" id="lastname"> </input>
+                <p id="lastname_error"></p> <!--Errors hier-->
+                <label class="Email" for="email">E-mail</label>
+                <input class="text" type="text" id="email"> </input>
+                <p id="email_error"></p> <!--Hier komen email errors zoals, geen of incorrecte email. Maak dit rood-->
+                <label class="Password" for="password">Password</label>
+                <input class="text" class="" type="password" id="password" oninput="checkPassword()"> </input>
+                <p id="password_error"></p> <!--Hier komen wachtwoord errors zoals, geen of incorrect wachtwoord. Maak dit rood-->
+                <label class="ConPassword" for="confirm_password">Confirm Password</label>
+                <input class="text" type="password" id="confirm_password" oninput="checkPassword()"> </input>
+                <p id="confirm_password_error"></p>  <!--Errors hier-->
+                <p>Tos hier?</p>
+                <input class="button" type="button" onclick="signup()" value="Sign Up"></input>
+                <p id="general_error"></p> <!--Hier komen general errors. Maak dit rood-->
+                <a href="../login">Ik heb al een account.</a>
+            </div>
+        </div>
     </div>
+    <?php
+    require("../footer.php")
+    ?>
 </body>
 </html>
