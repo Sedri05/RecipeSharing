e9865163893db772fe23628b4044a58255be5ecc
Senne <sedri051@gmail.com>
2024-04-26 15:51:39 +0200

    Search?

--- public/header.php
+++ public/header.php
@@ -1,5 +1,5 @@
-<link href="/header-style.css" rel="stylesheet" >
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" >
+<link href="/header-style.css" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0">
 <div class="header-container">
     <nav class="header-nav">
         <div class="header-flex">
@@ -8,14 +8,16 @@
             </div>
             <div class="header-flex-item"><a href="/search">Tags</a></div>
             <div class="header-flex-item"><a href="/about">Over Ons</a></div>
-            <?php if (isset($_SESSION["logged_in"])) {?>
+            <?php if (isset($_SESSION["logged_in"])) { ?>
                 <div class="header-flex-item"><a href="/account">Account</a></div>
-            <?php } else {?>
+            <?php } else { ?>
                 <div class="header-flex-item"><a href="/login">Inloggen</a></div>
             <?php } ?>
             <div class="header-flex-item-search">
-                    <span class="search-icon material-symbols-outlined">search</span>
-                    <input class="search-input" type="text" placeholder="zoeken">
+                <span class="search-icon material-symbols-outlined">search</span>
+                <form action="/search">
+                    <input class="search-input" type="text" placeholder="zoeken" name="search">
+                </form>
             </div>
         </div>
     </nav>
--- public/search/index.php
+++ public/search/index.php
@@ -1,18 +1,95 @@
 <?php session_start() ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="/reset.css" rel="stylesheet">
+    <title>Search results</title>
+    <script src="scripts.js"></script>
+    <link href="/reset.css" rel="stylesheet" />
+    <link href="style.css" rel="stylesheet" />
 </head>
+
 <body>
     <div class="wrapper">
-        <?php 
-        require("../header.php")
+        <?php
+        require("../header.php");
+        if (!isset($_GET["search"]) && !isset($_GET["tags"])) { ?>
+            <div class="container">
+                <div class="results">
+                    <p> Please enter a search query. </p>
+                </div>
+            </div></div>
+            
+            <?php require("../footer.php") ?>
+        <?php die();
+        }
+        require_once("../../private/database.php");
+        $database = new Database();
+        $data = $database->select("SELECT ReceptID, Title FROM recept");
+        $search_str = $_GET["search"];
+        /*usort($data, function ($a, $b) use ($search_str){
+            $a = $a["Title"];
+            $b = $b["Title"];
+            if ($a == $b) return 0;
+            elseif ($a == $search_str) return -1;
+            elseif ($b == $search_str) return 1;
+            return levenshtein($a, $search_str) <=> levenshtein($b, $search_str);
+        });*/
+        $distances = [];
+        foreach ($data as $d){
+            $distances[] = [$d, compareSentences($d["Title"], $search_str)];
+        }
+
+        usort($distances, function($a, $b) {
+            return $a[1] - $b[1]; // Ascending order
+        });
+
+
+        print_r($distances);
+        
         ?>
+        <div class="container">
+            <div class="results">
+                <h1 class="search-title">Search Results for: </h1>
+            </div>
+        </div>
     </div>
     <?php require("../footer.php") ?>
 </body>
-</html>     
+
+</html>
+
+<?php 
+function compareSentences($sentence1, $sentence2) {
+    // Split sentences into words
+    $words1 = explode(" ", $sentence1);
+    $words2 = explode(" ", $sentence2);
+
+    // Variable to hold the total Levenshtein distance
+    $totalDistance = 0;
+
+    // Loop through each word in the first sentence
+    foreach ($words1 as $word1) {
+        $minDistance = PHP_INT_MAX;
+
+        // Compare it to each word in the second sentence
+        foreach ($words2 as $word2) {
+            // Calculate Levenshtein distance
+            $levDistance = levenshtein($word1, $word2);
+
+            // If this distance is less than the current minimum, update minimum
+            if ($levDistance < $minDistance) {
+                $minDistance = $levDistance;
+            }
+        }
+
+        // Add the minimum distance for this word to the total distance
+        $totalDistance += $minDistance;
+    }
+
+    // Return the total Levenshtein distance
+    return $totalDistance;
+}
+?>
