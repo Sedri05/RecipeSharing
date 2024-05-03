59d856adaa610a7fe68e9a7b1f9ec6b49fe6803e
Szymon100 <szymon.bartus04@gmail.com>
2024-05-03 15:52:41 +0200

    aan new.php gewerkt

--- public/recept/new/new.php
+++ public/recept/new/new.php
@@ -1,3 +1,11 @@
-<?php session_start()
+<?php session_start();
+    require_once("../../private/database.php");
+    $Database = new Database;
+    $title = $_POST["Title"];
 
+
+    $Database->insert("INSERT INTO recepten(recept) 
+            VALUES ('?'?''?'')");
+
+    mysqli_query(,)
 ?>
