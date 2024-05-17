646b52b64bf69dacd00cdb3ffa13175797173913
Senne <sedri051@gmail.com>
2024-05-17 15:25:28 +0200

    Semi working search function

--- public/search/index.php
+++ public/search/index.php
@@ -20,69 +20,84 @@
                 <div class="results">
                     <p> Please enter a search query. </p>
                 </div>
-            </div></div>
-            
-            <?php require("../footer.php") ?>
-        <?php die();
+            </div>
+        </div>
+            <?php require("../footer.php");
+            die();
         }
         require_once("../../private/database.php");
         $database = new Database();
-        $data = $database->select("SELECT ReceptID, Title FROM recept");
-        $search_str = $_GET["search"];
-        /*usort($data, function ($a, $b) use ($search_str){
-            $a = $a["Title"];
-            $b = $b["Title"];
-            if ($a == $b) return 0;
-            elseif ($a == $search_str) return -1;
-            elseif ($b == $search_str) return 1;
-            return levenshtein($a, $search_str) <=> levenshtein($b, $search_str);
-        });*/
+        $data = $database->select("SELECT ReceptID, Title, Personen FROM recept", [], false);
+        $tags = $database->select("SELECT tr.ReceptID, t.Tagname FROM tagsrecept tr LEFT JOIN tag t ON tr.TagID = t.TagID", [], false);
+        $ingredients = $database->select("SELECT tr.ReceptID, t.Ingredient FROM ingredientrecept tr LEFT JOIN ingredient t ON tr.IngredientID = t.IngredientID; ");
+        $result = [];
 
-        
-        $distances = [];
-        foreach ($data as $d){
-            $distances[] = [$d, compareSentences($d["Title"], $search_str)];
+        foreach ($data as $recipe) {
+            $receptID = $recipe["ReceptID"];
+            $result[$receptID] = $recipe;
+            $result[$receptID]["Tags"] = [];
         }
 
-        usort($distances, function($a, $b) {
+        foreach ($ingredients as $ingr) {
+            $receptID = $ingr["ReceptID"];
+            if (isset($result[$receptID])) {
+                $result[$receptID]["Ingredient"][] = $ingr["Ingredient"];
+            }
+        }
+
+        foreach ($tags as $tag) {
+            $receptID = $tag["ReceptID"];
+            if (isset($result[$receptID])) {
+                $result[$receptID]["Tags"][] = $tag["Tagname"];
+            }
+        }
+
+        $search_str = $_GET["search"];
+
+        $distances = [];
+        foreach ($result as $d) {
+            $distances[] = [$d, compareRecept($d, $search_str)];
+        }
+
+        usort($distances, function ($a, $b) {
             return $a[1] - $b[1]; // Ascending order
         });
 
-
-        print_r($distances);
-        
+?>
+<div class="container">
+    <div class="results">
+        <h1 class="search-title">Search Results for: <?php echo $search_str ?></h1>
+        <?php
+        foreach ($distances as $recept) {
+            echo "<p> Title: " . $recept[0]["Title"] . "</p>";
+        }
         ?>
-        <div class="container">
-            <div class="results">
-                <h1 class="search-title">Search Results for: </h1>
-            </div>
-        </div>
     </div>
-    <?php require("../footer.php") ?>
+</div>
+</div>
+<?php require("../footer.php") ?>
 </body>
 
 </html>
 
-<?php 
-function compareSentences($sentence1, $sentence2) {
-    $words1 = explode(" ", $sentence1);
-    $words2 = explode(" ", $sentence2);
+<?php
+
+function compareRecept($recept1, $recept2)
+{
+    $searchables2 = array_merge(explode(" ", $recept1["Title"]), $recept1["Tags"], $recept1["Ingredient"]);
+    $searchables2 = array_map("strtolower", $searchables2);
+    $searchables1 = explode(" ", strtolower($recept2));
 
     $totalDistance = 0;
-
-    foreach ($words1 as $word1) {
+    foreach ($searchables1 as $s1) {
         $minDistance = PHP_INT_MAX;
-
-        foreach ($words2 as $word2) {
-            $levDistance = levenshtein($word1, $word2);
+        foreach ($searchables2 as $s2) {
+            $levDistance = levenshtein($s1, $s2);
             if ($levDistance < $minDistance) {
                 $minDistance = $levDistance;
             }
         }
-
         $totalDistance += $minDistance;
     }
-
     return $totalDistance;
 }
-?>
