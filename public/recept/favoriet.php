d36113a958b611ca03e55ddb8619a2e6fc9e9949
Senne <sedri051@gmail.com>
2024-05-17 13:36:39 +0200

    Favorieten en reviews

---
+++ public/recept/favoriet.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+if (!isset($_SESSION["logged_in"])) { ?>
+    <p> You are not logged in. Click <a href="../login/">Here</a> to log in.</p>
+<?php
+    die();
+}
+
+$user = $_SESSION["user"];
+$id = $_POST["id"];
+require("../../private/database.php");
+$database = new Database();
+$result = $database->select("SELECT `ReceptID`, `GebruikerID` FROM `favoriet` WHERE `ReceptID` = ? AND `GebruikerID` = ?", ["ii", [$id, $user]]);
+if (count($result) > 0){ 
+    $database->delete("DELETE FROM `favoriet` WHERE `ReceptID` = ? AND `GebruikerID` = ?", ["ii", [$id, $user]]);
+} else {
+    $database->insert("INSERT INTO `favoriet`(`ReceptID`, `GebruikerID`) VALUES (?,?)", ["ii", [$id, $user]]);
+}
+echo json_encode(array("success" => "success"));
+?>
--- public/recept/index.php
+++ public/recept/index.php
@@ -8,7 +8,8 @@
     <script src="scripts.js"></script>
     <link href="/reset.css" rel="stylesheet" />
     <link href="style.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 
 <body>
@@ -20,6 +21,7 @@
         }
         require_once "../../private/database.php";
         $recept_id = intval($_GET["recept"]);
+        //echo "<script> var ID = " . $recept_id . "; </script>";
         $database = new Database();
         $recept_info = $database->select_one("SELECT * FROM `recept` WHERE `ReceptID` = ?", ["i", [$recept_id]], false);
         $recept_tags = $database->select("SELECT `Tagname` FROM `tag` t LEFT JOIN `tagsrecept` r ON t.TagID = r.TagID WHERE r.ReceptID = ?", ["i", [$recept_id]], false);
@@ -27,15 +29,21 @@
         $creator = $database->select_one("SELECT `Naam`, `Achternaam` FROM `gebruiker` WHERE `GebruikerID` = ?", ["i", [$recept_info["GebruikerID"]]], false);
         $review = $database->select("SELECT * FROM `review` WHERE `ReceptID` = ?", ["i", [$recept_id]], false);
         $type = $database->select_one("SELECT `Maaltijdtype` FROM `maaltijdtype` WHERE `MaaltijdtypeID` = ?;", ["i", [$recept_info["MaaltijdtypeID"]]]);
+        $reviews = $database->select("SELECT `ReviewID`, g.Naam, g.Achternaam, `Score`, `Reviewtekst`, `Date` FROM `review` r LEFT JOIN `gebruiker` g ON r.GebruikerID = g.GebruikerID WHERE `ReceptID` = ? ORDER BY `Date`", ["i", [$recept_id]]);
+        
+        $favoriet = false;
+        if (isset($_SESSION["logged_in"])) {
+            $temp = $database->select("SELECT `ReceptID`, `GebruikerID` FROM `favoriet` WHERE `ReceptID` = ? AND `GebruikerID` = ?", ["ii", [$recept_id, $_SESSION["user"]]]);
+            $favoriet = count($temp) > 0;
+        }
         ?>
+        <title><?php echo $recept_info["Title"]; ?></title>
         <div class="centered">
             <div class="container">
                 <div class="title-div">
-                    <div class="title">
-                        <h1> <?php echo $recept_info["Title"]; ?> </h1>
-                    </div>
-                    <div class="creator">
-                        <p> <?php echo $creator["Naam"] . " " . $creator["Achternaam"] ?> </p>
+                    <h1> <?php echo $recept_info["Title"]; ?> </h1>
+                    <div class="favorite" onclick="favorite(<?php echo $recept_id; ?>)">
+                        <span id="favorite" class="fixed"><?php echo (!$favoriet) ?  "favorite" : "heart_check" ?></span>
                     </div>
                 </div>
                 <div class="info">
@@ -50,6 +58,15 @@
                     </div>
                     <div class="info-box">
                         <p> Aantal personen: <?php echo $recept_info["Personen"]; ?> </p>
+                    </div>
+                    <!--
+                    <div class="info-icon right">
+                        <span class="material-symbols-outlined fixed"> favorite
+                        </span>
+                    </div>
+                    -->
+                    <div class="info-box right">
+                        <p> <?php echo $creator["Naam"] . " " . $creator["Achternaam"] ?> </p>
                     </div>
                 </div>
                 <div class="ingr-tags-div">
@@ -91,9 +108,13 @@
                     <div class="review-div">
                         <p class="review-title">Reviews</p>
                         <div class="review">
-                            <p>
-
-                            </p>
+                            <?php 
+                            foreach($reviews as $review){ ?>
+                            <div class="user-review">
+                                <h3 class="review-user"><?php echo $review["Naam"] . " " . $review["Achternaam"] . " - " . $review["Score"] . "/5" ?></h3>
+                                <p class="review-text"><?php echo $review["Reviewtekst"]?></p>
+                            </div>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
