faec2b13a9ef9aaaab15d28c8dc179dd34551341
Senne <sedri051@gmail.com>
2024-05-28 18:05:00 +0200

    Home page fixed

--- public/index.php
+++ public/index.php
@@ -21,30 +21,29 @@
             $database = new Database();
 
             $recepten = $database->select("SELECT `ReceptID`, `Title`, `Moeilijkheid`,`Berijdingstijd`,`Personen`, `Foto`, `Date` FROM `recept` ORDER by Date DESC;");
-            ?> <div class="center">
+            ?>
+            <div class="center">
                 <h3 class="par1">Recente recepten:</h3>
                 <?php
                 foreach ($recepten as $recept) {
                 ?>
-                    <div class="recept-row">
-                        <a href="/recept/?recept=<?php echo $recept["ReceptID"] ?>">
-                            <div class="column">
-                                <img src="<?php echo $database->get_image($recept["ReceptID"]) ?>">
-                            </div>
-                        </a>
+                    <a href="/recept/?recept=<?php echo $recept["ReceptID"] ?>" class="recept-row">
+                        <div class="column">
+                            <img src="<?php echo $database->get_image($recept["ReceptID"]) ?>">
+                        </div>
+
                         <div class="recept-info-column">
                             <h2 class="recept-info-title"> <?php echo $recept["Title"] ?> </h2>
                             <p class="recept-info"> Moeilijkheid: <?php echo $recept["Moeilijkheid"] ?> </p>
-                            <p class="recept-info"> Tijd: <?php echo $recept["Berijdingstijd"] ?> </p>
-                            <p class="recept-info"> Aantal Personen <?php echo $recept["Personen"] ?> </p>
+                            <p class="recept-info"> Tijd: <?php echo $recept["Berijdingstijd"] ?> Minuten</p>
+                            <p class="recept-info"> Voor <?php echo $recept["Personen"] ?> Personen</p>
                         </div>
-
-                    </div>
+                    </a>
                 <?php
                 }
                 ?>
             </div>
-            <a href="/recept/new/index.php">Recepten toevoegen</a>
+            <!--<a href="/recept/new/index.php">Recepten toevoegen</a>-->
         </div>
     </div>
     <?php require("footer.php") ?>
