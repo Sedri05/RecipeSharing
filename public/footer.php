50504d40d34e727558f2e0c899336307176e2cfa
Senne <sedri051@gmail.com>
2024-06-06 12:25:54 +0200

    fix

--- public/footer.php
+++ public/footer.php
@@ -2,7 +2,7 @@
 <footer>
     <div class="footer-nav">
         <ul>
-            <li><a href="index.php">Home</a></li>
+            <li><a href="/index.php">Home</a></li>
             <li><a href="/about">Over Ons</a></li>
         </ul>
     </div>
